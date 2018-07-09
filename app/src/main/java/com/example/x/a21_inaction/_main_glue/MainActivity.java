ff5e92c6c4025becce28615e9958be464aeb58d6
X-key <imohammedibrahim01@gmail.com>
2018-07-09 13:26:53 +0200

    create packages to structure the application

--- app/src/main/java/com/example/x/a21_inaction/_main_glue/MainActivity.java
+++ app/src/main/java/com/example/x/a21_inaction/_main_glue/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.x.a21_inaction;
+package com.example.x.a21_inaction._main_glue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.x.a21_inaction.R;
 
 public class MainActivity extends AppCompatActivity {
 
