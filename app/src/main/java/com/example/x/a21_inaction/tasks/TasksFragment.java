7b62227fac2f08534df2365764400a1132512ed3
X-key <imohammedibrahim01@gmail.com>
2018-07-09 23:38:56 +0200

    add tasks divider decoration

--- app/src/main/java/com/example/x/a21_inaction/tasks/TasksFragment.java
+++ app/src/main/java/com/example/x/a21_inaction/tasks/TasksFragment.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -53,7 +55,12 @@ public class TasksFragment extends Fragment implements TaskAdapter.OnListItemCli
         View view = inflater.inflate(R.layout.fragment_tasks, container, false);
         ButterKnife.bind(this, view);
 
+        //setup divider and recyclerView
+        DividerItemDecoration decoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        decoration.setDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.tasks_divider));
+
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        tasksRecyclerView.addItemDecoration(decoration);
         tasksRecyclerView.setAdapter(adapter);
 
         setupViewModel();
