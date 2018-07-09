db300e320b2c31a1adfe56b70017535123c1d2ce
X-key <imohammedibrahim01@gmail.com>
2018-07-09 14:04:28 +0200

    add counterTextView, mainTabLayout, mainViewPager to MainActivity

--- app/src/main/java/com/example/x/a21_inaction/_main_glue/MainActivity.java
+++ app/src/main/java/com/example/x/a21_inaction/_main_glue/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.x.a21_inaction._main_glue;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.x.a21_inaction.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.counter_textView)
+    TextView counterTextView;
+    @BindView(R.id.main_tabLayout)
+    TabLayout mainTabLayout;
+    @BindView(R.id.main_viewPager)
+    ViewPager mainViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
