51e7ff5f7812283f0bbd34e5b8d9247fb682f4e1
X-key <imohammedibrahim01@gmail.com>
2018-07-09 14:41:45 +0200

    glue the three fragments in MainActivity

--- app/src/main/java/com/example/x/a21_inaction/_main_glue/MainActivity.java
+++ app/src/main/java/com/example/x/a21_inaction/_main_glue/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.x.a21_inaction.R;
+import com.example.x.a21_inaction.achievements.AchievementsFragment;
+import com.example.x.a21_inaction.day_zero.DayZeroFragment;
+import com.example.x.a21_inaction.tasks.TasksFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        //glue the three fragments
+        MainFragmentPagerAdapter adapter = new MainFragmentPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TasksFragment(), "tasks");
+        adapter.addFragment(new AchievementsFragment(), "achievements");
+        adapter.addFragment(new DayZeroFragment(), "day zero");
+
+        mainViewPager.setAdapter(adapter);
+        mainTabLayout.setupWithViewPager(mainViewPager);
     }
 }
